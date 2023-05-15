7346fc76be3537352d37e7148961d0a27e409254
Benedetta-fabbri <Benedetta11.Fabbri@emea.nttdata.com>
2023-05-15 17:26:43 +0200

    initial configuration

--- src/main/java/it/gov/pagopa/merchant/controller/MerchantControllerImpl.java
+++ src/main/java/it/gov/pagopa/merchant/controller/MerchantControllerImpl.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.merchant.controller;
 
+import it.gov.pagopa.merchant.service.MerchantService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.RestController;
 
