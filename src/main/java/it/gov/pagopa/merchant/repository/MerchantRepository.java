a07c0355478b6ca36f040ef869b980658a6b616f
Giulia Tremolada <giulia.tremolada@nttdata.com>
2023-05-17 18:12:19 +0200

    fix refactor MerchantInitiative

--- src/main/java/it/gov/pagopa/merchant/repository/MerchantRepository.java
+++ src/main/java/it/gov/pagopa/merchant/repository/MerchantRepository.java
@@ -1,10 +1,10 @@
 package it.gov.pagopa.merchant.repository;
 
-import it.gov.pagopa.merchant.model.Merchant;
+import it.gov.pagopa.merchant.model.MerchantInitiative;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
-public interface MerchantRepository extends MongoRepository<Merchant, String>,MerchantRepositoryExtended {
-    List<Merchant> findByInitiativeId(String initiativeId);
+public interface MerchantRepository extends MongoRepository<MerchantInitiative, String>,MerchantRepositoryExtended {
+    List<MerchantInitiative> findByInitiativeId(String initiativeId);
 }
--- src/main/java/it/gov/pagopa/merchant/repository/MerchantRepositoryExtendedImpl.java
+++ src/main/java/it/gov/pagopa/merchant/repository/MerchantRepositoryExtendedImpl.java
@@ -1,6 +1,6 @@
 package it.gov.pagopa.merchant.repository;
 
-import it.gov.pagopa.merchant.model.Merchant;
+import it.gov.pagopa.merchant.model.MerchantInitiative;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
@@ -18,22 +18,22 @@ public class MerchantRepositoryExtendedImpl implements MerchantRepositoryExtende
     }
 
     @Override
-    public List<Merchant> findByFilter(Criteria criteria, Pageable pageable) {
-        return mongoTemplate.find(Query.query(criteria).with(this.getPageable(pageable)), Merchant.class);
+    public List<MerchantInitiative> findByFilter(Criteria criteria, Pageable pageable) {
+        return mongoTemplate.find(Query.query(criteria).with(this.getPageable(pageable)), MerchantInitiative.class);
     }
 
     @Override
     public Criteria getCriteria(String initiativeId, String fiscalCode) {
-        Criteria criteria = Criteria.where(Merchant.Fields.initiativeId).is(initiativeId);
+        Criteria criteria = Criteria.where(MerchantInitiative.Fields.initiativeId).is(initiativeId);
         if (fiscalCode != null) {
-            criteria.and(Merchant.Fields.fiscalCode).is(fiscalCode);
+            criteria.and(MerchantInitiative.Fields.fiscalCode).is(fiscalCode);
         }
         return criteria;
     }
 
     @Override
     public long getCount(Criteria criteria) {
-        return mongoTemplate.count(Query.query(criteria), Merchant.class);
+        return mongoTemplate.count(Query.query(criteria), MerchantInitiative.class);
     }
 
     private Pageable getPageable(Pageable pageable) {
