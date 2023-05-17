820596ad9b926ff0f442af047d9cc3d34edd08f2
Ranaldo Daniele <Daniele.Ranaldo@emea.nttdata.com>
2023-05-17 16:28:41 +0200

    [IDP-942] added model and repository

---
+++ src/main/java/it/gov/pagopa/merchant/model/Merchant.java
@@ -0,0 +1,33 @@
+package it.gov.pagopa.merchant.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.experimental.FieldNameConstants;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@FieldNameConstants
+@Document(collection = "merchant")
+public class Merchant {
+    private String initiativeId;
+    private String businessName; //ragione sociale
+    private String merchantName; //nome insegna
+    private String legalOfficeAddress;
+    private String legalOfficeMunicipality; // comune sede legale
+    private String legalOfficeProvince;
+    private String legalOfficeZipCode;
+    private String certifiedEmail;
+    private String fiscalCode;
+    private String vatNumber;
+    private String status;
+    private String iban;
+    private LocalDateTime creationDate;
+    private LocalDateTime updateDate;
+}
