c9519c461278ab49720ab5f9c47366f08f0c1b06
frtrinca <francesco.trinca@nttdata.com>
2023-07-06 15:10:00 +0200

    [IDP-1267] New controller for upload file merchant

---
+++ src/main/java/it/gov/pagopa/merchant/controller/acquirer/AcquirerController.java
@@ -0,0 +1,29 @@
+package it.gov.pagopa.merchant.controller.acquirer;
+
+import io.swagger.v3.oas.annotations.Operation;
+import it.gov.pagopa.merchant.dto.InitiativeDTO;
+import it.gov.pagopa.merchant.dto.MerchantUpdateDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/idpay/merchant/acquirer")
+public interface AcquirerController {
+
+    @Operation(summary = "Returns the list of initiatives of a specific merchant")
+    @GetMapping("/initiatives")
+    @ResponseStatus(code = HttpStatus.OK)
+    List<InitiativeDTO> getMerchantInitiativeList(@RequestHeader("x-merchant-id") String merchantId);
+
+    @Operation(summary = "Uploads the merchants file")
+    @PutMapping("/{acquirerId}/initiative/{initiativeId}/upload")
+    @ResponseStatus(code = HttpStatus.OK)
+    ResponseEntity<MerchantUpdateDTO> uploadMerchantFile(
+            @RequestParam("file") MultipartFile file,
+            @PathVariable("acquirerId") String organizationId,
+            @PathVariable("initiativeId") String initiativeId);
+}
