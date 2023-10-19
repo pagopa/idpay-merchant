9f037e183e5d5ea0f86ec47617f927acf2d13471
anttorre <antonio.torre@nttdata.com>
2023-10-19 17:26:23 +0200

    IDP-2003 fix Mongo Retry When Anonymous Repo

--- src/main/java/it/gov/pagopa/common/mongo/retry/MongoRequestRateTooLargeAutomaticRetryAspect.java
+++ src/main/java/it/gov/pagopa/common/mongo/retry/MongoRequestRateTooLargeAutomaticRetryAspect.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.common.mongo.retry;
 
+import lombok.Generated;
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
@@ -42,15 +43,15 @@ public class MongoRequestRateTooLargeAutomaticRetryAspect {
         this.maxMillisElapsedBatch = maxMillisElapsedBatch;
     }
 
-    // BEGIN-NOSCAN
+    @Generated
     @Pointcut("execution(* org.springframework.data.mongodb.repository.*MongoRepository+.*(..))")
     public void inSpringRepositoryClass() {
     }
 
+    @Generated
     @Pointcut("within(it.gov.pagopa..*Repository*)")
     public void inRepositoryClass() {
     }
-    // END-NOSCAN
 
     @Around("inRepositoryClass() or inSpringRepositoryClass()")
     public Object decorateRepositoryMethods(ProceedingJoinPoint pjp) throws Throwable {
