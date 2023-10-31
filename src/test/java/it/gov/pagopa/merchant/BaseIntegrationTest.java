a90821c61e08887c9e8180d5f0650c1852f6af0d
frtrinca <francesco.trinca@nttdata.com>
2023-10-31 17:14:45 +0100

    Fix

--- src/test/java/it/gov/pagopa/merchant/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/merchant/BaseIntegrationTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import it.gov.pagopa.common.kafka.KafkaTestUtilitiesService;
 import it.gov.pagopa.common.mongo.MongoTestUtilitiesService;
+import it.gov.pagopa.common.mongo.singleinstance.AutoConfigureSingleInstanceMongodb;
 import it.gov.pagopa.common.utils.TestIntegrationUtils;
 import it.gov.pagopa.merchant.connector.file_storage.AzureBlobClient;
 import jakarta.annotation.PostConstruct;
@@ -68,6 +69,7 @@ import javax.management.MalformedObjectNameException;
         })
 @AutoConfigureMockMvc
 @AutoConfigureWireMock(stubs = "classpath:/mappings", port = 0)
+@AutoConfigureSingleInstanceMongodb
 public abstract class BaseIntegrationTest {
 
     @Autowired
