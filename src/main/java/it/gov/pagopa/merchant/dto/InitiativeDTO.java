6cf3c14ca228f9ff2d67dff10be48c5141a79820
frtrinca <francesco.trinca@nttdata.com>
2023-11-09 15:35:37 +0100

    Rimozione validazione response

--- src/main/java/it/gov/pagopa/merchant/dto/InitiativeDTO.java
+++ src/main/java/it/gov/pagopa/merchant/dto/InitiativeDTO.java
@@ -1,6 +1,5 @@
 package it.gov.pagopa.merchant.dto;
 
-import jakarta.validation.constraints.NotBlank;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -15,9 +14,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 public class InitiativeDTO {
-    @NotBlank
     private String initiativeId;
-    @NotBlank
     private String initiativeName;
     private String organizationId;
     private String organizationName;
