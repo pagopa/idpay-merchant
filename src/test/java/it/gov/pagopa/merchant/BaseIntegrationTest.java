20610b3d5c4c19c02f3d54c0c3d513d3559fe2b4
antonioT90 <34568575+antonioT90@users.noreply.github.com>
2024-01-11 16:28:18 +0100

    fix: IDP-2196 fix Cve (#95)

--- src/test/java/it/gov/pagopa/merchant/BaseIntegrationTest.java
+++ src/test/java/it/gov/pagopa/merchant/BaseIntegrationTest.java
@@ -31,10 +31,6 @@ import javax.management.MalformedObjectNameException;
 }, controlledShutdown = true)
 @TestPropertySource(
         properties = {
-                // even if enabled into application.yml, spring test will not load it
-                // https://docs.spring.io/spring-boot/docs/current/reference/html/features.html#features.testing.spring-boot-applications.jmx
-                "spring.jmx.enabled=true",
-
                 // region mongodb
                 "logging.level.org.mongodb.driver=WARN",
                 "logging.level.de.flapdoodle.embed.mongo.spring.autoconfigure=WARN",
@@ -53,7 +49,6 @@ import javax.management.MalformedObjectNameException;
                 "logging.level.state.change.logger=WARN",
                 "spring.cloud.stream.kafka.binder.configuration.security.protocol=PLAINTEXT",
                 "spring.kafka.bootstrap-servers=${spring.embedded.kafka.brokers}",
-                "spring.cloud.stream.kafka.binder.zkNodes=${spring.embedded.zookeeper.connect}",
                 "spring.cloud.stream.binders.kafka-reward-notification-upload.environment.spring.cloud.stream.kafka.binder.brokers=${spring.embedded.kafka.brokers}",
                 "spring.cloud.stream.binders.kafka-errors.environment.spring.cloud.stream.kafka.binder.brokers=${spring.embedded.kafka.brokers}",
                 "spring.cloud.stream.binders.kafka-initiative.environment.spring.cloud.stream.kafka.binder.brokers=${spring.embedded.kafka.brokers}",
