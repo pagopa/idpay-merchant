74d6c460598490562055cf5da01f8fdec276b5a8
Vitolo-Andrea <andrea.vitolo@emeal.nttdata.com>
2024-09-18 11:41:50 +0200

    chore: fix-cve

--- src/test/java/it/gov/pagopa/common/kafka/KafkaTestUtilitiesService.java
+++ src/test/java/it/gov/pagopa/common/kafka/KafkaTestUtilitiesService.java
@@ -47,7 +47,7 @@ import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
 
 /** Utilities when performing Kafka tests */
-@ConditionalOnClass(EmbeddedKafkaBroker.class)
+@ConditionalOnClass(EmbeddedKafkaKraftBroker.class)
 @Service
 public class KafkaTestUtilitiesService {
 
@@ -219,10 +219,10 @@ public class KafkaTestUtilitiesService {
             headers = Stream.concat(
                             Arrays.stream(additionalHeaders),
                             StreamSupport.stream(headers.spliterator(), false))
-                    .collect(Collectors.toList());
+                    .toList();
         }
-        ProducerRecord<byte[], byte[]> record = new ProducerRecord<>(topic, partition, key == null ? null : key.getBytes(StandardCharsets.UTF_8), payload.getBytes(StandardCharsets.UTF_8), headers);
-        template.send(record);
+        ProducerRecord<byte[], byte[]> rec = new ProducerRecord<>(topic, partition, key == null ? null : key.getBytes(StandardCharsets.UTF_8), payload.getBytes(StandardCharsets.UTF_8), headers);
+        template.send(rec);
     }
 //endregion
 
@@ -278,13 +278,13 @@ public class KafkaTestUtilitiesService {
     //region error topic
     public void checkErrorsPublished(String topicErrors, Pattern errorUseCaseIdPatternMatch, int expectedErrorMessagesNumber, long maxWaitingMs, List<Pair<Supplier<String>, java.util.function.Consumer<ConsumerRecord<String, String>>>> errorUseCases) {
         final List<ConsumerRecord<String, String>> errors = consumeMessages(topicErrors, expectedErrorMessagesNumber, maxWaitingMs);
-        for (final ConsumerRecord<String, String> record : errors) {
-            final Matcher matcher = errorUseCaseIdPatternMatch.matcher(record.value());
+        for (final ConsumerRecord<String, String> rec : errors) {
+            final Matcher matcher = errorUseCaseIdPatternMatch.matcher(rec.value());
             int useCaseId = matcher.find() ? Integer.parseInt(matcher.group(1)) : -1;
             if (useCaseId == -1) {
-                throw new IllegalStateException("UseCaseId not recognized! " + record.value());
+                throw new IllegalStateException("UseCaseId not recognized! " + rec.value());
             }
-            errorUseCases.get(useCaseId).getSecond().accept(record);
+            errorUseCases.get(useCaseId).getSecond().accept(rec);
         }
     }
 
