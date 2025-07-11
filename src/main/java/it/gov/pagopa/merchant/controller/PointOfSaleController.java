0ead1c14e3907e26da479c6961fd58fddee7fce5
fedcurciorubertini <federico.curciorubertini@nttdata.com>
2025-07-11 14:45:55 +0200

    added default sorting

--- src/main/java/it/gov/pagopa/merchant/controller/PointOfSaleController.java
+++ src/main/java/it/gov/pagopa/merchant/controller/PointOfSaleController.java
@@ -3,6 +3,7 @@ package it.gov.pagopa.merchant.controller;
 import io.swagger.v3.oas.annotations.Operation;
 import it.gov.pagopa.merchant.dto.PointOfSaleListDTO;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.web.PageableDefault;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -19,5 +20,7 @@ public interface PointOfSaleController {
             @RequestParam(required = false) String city,
             @RequestParam(required = false) String address,
             @RequestParam(required = false) String contactName,
-            @PageableDefault(size = 8) Pageable pageable);
+            @PageableDefault(size = 8,
+             sort = "franchiseName",
+             direction = Sort.Direction.ASC) Pageable pageable);
 }
