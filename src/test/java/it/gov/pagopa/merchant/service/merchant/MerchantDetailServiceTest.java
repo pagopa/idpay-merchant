fe79e890622a30137a8df5d28dc620e19860e613
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-07-28 20:59:29 +0200

    Removed unused import

--- src/test/java/it/gov/pagopa/merchant/service/merchant/MerchantDetailServiceTest.java
+++ src/test/java/it/gov/pagopa/merchant/service/merchant/MerchantDetailServiceTest.java
@@ -21,7 +21,6 @@ import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
