fe5e4666c195c1ac595c10fd51045988f3136e89
Christian Geovese <cgenoves@emeal.nttdata.com>
2025-07-29 17:28:42 +0200

    add junit test case

--- src/test/java/it/gov/pagopa/merchant/controller/PointOfSaleControllerImplTest.java
+++ src/test/java/it/gov/pagopa/merchant/controller/PointOfSaleControllerImplTest.java
@@ -3,7 +3,9 @@ package it.gov.pagopa.merchant.controller;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import it.gov.pagopa.common.config.JsonConfig;
 import it.gov.pagopa.merchant.configuration.ServiceExceptionConfig;
+import it.gov.pagopa.merchant.dto.MerchantDetailDTO;
 import it.gov.pagopa.merchant.dto.pointofsales.PointOfSaleDTO;
+import it.gov.pagopa.merchant.dto.pointofsales.PointOfSaleDetailDTO;
 import it.gov.pagopa.merchant.mapper.PointOfSaleDTOMapper;
 import it.gov.pagopa.merchant.model.PointOfSale;
 import it.gov.pagopa.merchant.service.merchant.MerchantDetailService;
@@ -13,7 +15,6 @@ import it.gov.pagopa.merchant.test.fakers.PointOfSaleFaker;
 import it.gov.pagopa.merchant.utils.validator.PointOfSaleValidator;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.context.annotation.Import;
@@ -31,6 +32,7 @@ import java.util.List;
 
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 
 @WebMvcTest(PointOfSaleControllerImpl.class)
@@ -80,10 +82,10 @@ class PointOfSaleControllerImplTest {
     @Test
     void getPointOfSalesListOK() throws Exception {
         PointOfSale pointOfSale = PointOfSaleFaker.mockInstance();
-        PageRequest pageRequest = PageRequest.of(0,10);
+        PageRequest pageRequest = PageRequest.of(0, 10);
         Page<PointOfSale> expectedPage = new PageImpl<>(List.of(pointOfSale), pageRequest, 1);
 
-        Mockito.when(pointOfSaleService.getPointOfSalesList(any(),any(),any(),any(),any(),any())).thenReturn(expectedPage);
+        when(pointOfSaleService.getPointOfSalesList(any(), any(), any(), any(), any(), any())).thenReturn(expectedPage);
 
         MvcResult result =
                 mockMvc.perform(
@@ -94,4 +96,32 @@ class PointOfSaleControllerImplTest {
         Assertions.assertNotNull(result);
     }
 
+    @Test
+    void getPointOfSaleDetailByIdOK() throws Exception {
+
+        PointOfSale pointOfSale = PointOfSaleFaker.mockInstance();
+        PointOfSaleDTO pointOfSaleDTO = PointOfSaleDTOFaker.mockInstance();
+
+        MerchantDetailDTO merchantDetailDTO = MerchantDetailDTO.builder()
+                .vatNumber("12345678901")
+                .iban("IT60X0542811101000000123456")
+                .build();
+
+        PointOfSaleDetailDTO expectedDTO = PointOfSaleDetailDTO.builder()
+                .pointOfSale(pointOfSaleDTO)
+                .merchantDetail(merchantDetailDTO)
+                .build();
+
+        when(pointOfSaleService.getPointOfSaleById("POS_ID")).thenReturn(pointOfSale);
+        when(merchantDetailService.getMerchantDetail(MERCHANT_ID)).thenReturn(merchantDetailDTO);
+        when(mapper.pointOfSaleEntityToPointOfSaleDTO(pointOfSale)).thenReturn(pointOfSaleDTO);
+
+        MvcResult result = mockMvc.perform(
+                        MockMvcRequestBuilders.get(BASE_URL + "/" + MERCHANT_ID + "/point-of-sales/POS_ID")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andDo(print())
+                .andReturn();
+        Assertions.assertNotNull(result);
+    }
 }
