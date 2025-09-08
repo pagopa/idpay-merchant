6724c82333cde5f2e57b55a00c190b32f679d0d3
BaldiVi-1 <vittorio.baldi@emeal.nttdata.com>
2025-09-08 17:30:01 +0200

    change pointOfSalesId to pointOfSaleId attr keycloak

--- src/main/java/it/gov/pagopa/merchant/service/pointofsales/PointOfSaleServiceImpl.java
+++ src/main/java/it/gov/pagopa/merchant/service/pointofsales/PointOfSaleServiceImpl.java
@@ -257,7 +257,7 @@ public class PointOfSaleServiceImpl implements PointOfSaleService {
     // Custom attrs
     Map<String, List<String>> attrs = new HashMap<>();
     attrs.put("merchantId", List.of(pointOfSale.getMerchantId()));
-    attrs.put("pointOfSalesId", List.of(pointOfSale.getId().toString()));
+    attrs.put("pointOfSaleId", List.of(pointOfSale.getId().toString()));
     newUser.setAttributes(attrs);
 
     log.info("[KEYCLOAK] Attempting to create a new Keycloak user linked to Point of Sale ID {}",
