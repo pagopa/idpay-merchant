3fe6e03cfd43d6b108a48bcb729c83117e8d6d5a
EMORONGD6 <guido.moroni@emeal.nttdata.com>
2025-10-17 16:02:42 +0200

    fix: fixing new pos email check

--- src/main/java/it/gov/pagopa/merchant/repository/PointOfSaleRepository.java
+++ src/main/java/it/gov/pagopa/merchant/repository/PointOfSaleRepository.java
@@ -3,7 +3,6 @@ package it.gov.pagopa.merchant.repository;
 import it.gov.pagopa.merchant.model.PointOfSale;
 import java.util.List;
 import org.springframework.data.mongodb.repository.MongoRepository;
-import org.springframework.data.mongodb.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
