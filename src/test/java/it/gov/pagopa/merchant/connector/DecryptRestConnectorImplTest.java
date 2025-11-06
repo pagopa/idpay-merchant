87c968fce1aa4014fdc6e5bf1d455bffa4df1a85
leonardomagnante <lmagnant@nttdata.com>
2025-11-06 12:41:45 +0100

    fix test

--- src/test/java/it/gov/pagopa/merchant/connector/DecryptRestConnectorImplTest.java
+++ src/test/java/it/gov/pagopa/merchant/connector/DecryptRestConnectorImplTest.java
@@ -5,12 +5,10 @@ import it.gov.pagopa.merchant.connector.decrypt.DecryptRestConnectorImpl;
 import it.gov.pagopa.merchant.dto.DecryptCfDTO;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.*;
 
 @ExtendWith(MockitoExtension.class)
@@ -18,7 +16,6 @@ class DecryptRestConnectorImplTest {
 
     @Test
     void getPiiByToken_ok_delegatesToClientWithApiKeyAndReturnsResult() {
-        // arrange
         String apiKey = "test-api-key";
         String token = "tok-123";
         DecryptRest decryptRest = mock(DecryptRest.class);
@@ -26,29 +23,31 @@ class DecryptRestConnectorImplTest {
 
         DecryptCfDTO expected = new DecryptCfDTO();
 
-        when(decryptRest.getPiiByToken(token, eq(apiKey))).thenReturn(expected);
+        when(decryptRest.getPiiByToken(token, apiKey)).thenReturn(expected);
 
         DecryptCfDTO res = sut.getPiiByToken(token);
 
         assertThat(res).isSameAs(expected);
-        verify(decryptRest, times(1)).getPiiByToken(token, eq(apiKey));
+        verify(decryptRest).getPiiByToken(token, apiKey);
         verifyNoMoreInteractions(decryptRest);
     }
 
     @Test
     void getPiiByToken_ko_propagatesExceptionFromClient() {
-
         String apiKey = "test-api-key";
         String token = "tok-err";
-        DecryptRest decryptRest = Mockito.mock(DecryptRest.class);
+        DecryptRest decryptRest = mock(DecryptRest.class);
         DecryptRestConnectorImpl sut = new DecryptRestConnectorImpl(apiKey, decryptRest);
 
         RuntimeException boom = new RuntimeException("boom");
-        when(decryptRest.getPiiByToken((token), eq(apiKey))).thenThrow(boom);
+
+        when(decryptRest.getPiiByToken(token, apiKey)).thenThrow(boom);
 
         RuntimeException thrown = assertThrows(RuntimeException.class, () -> sut.getPiiByToken(token));
         assertThat(thrown).isSameAs(boom);
-        verify(decryptRest).getPiiByToken(token, eq(apiKey));
+
+        verify(decryptRest).getPiiByToken(token, apiKey);
         verifyNoMoreInteractions(decryptRest);
     }
+
 }
