b9e081e60d38574e3b38de78c5ab551a613617e7
leonardomagnante <lmagnant@nttdata.com>
2025-11-06 11:43:56 +0100

    Decrypt connector

--- src/main/java/it/gov/pagopa/merchant/configuration/RestConnectorConfig.java
+++ src/main/java/it/gov/pagopa/merchant/configuration/RestConnectorConfig.java
@@ -1,5 +1,6 @@
 package it.gov.pagopa.merchant.configuration;
 
+import it.gov.pagopa.merchant.connector.decrypt.DecryptRest;
 import it.gov.pagopa.merchant.connector.initiative.InitiativeRestClient;
 import it.gov.pagopa.merchant.connector.payment.PaymentRestClient;
 import it.gov.pagopa.merchant.connector.transaction.TransactionRestClient;
@@ -8,6 +9,7 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 @EnableFeignClients(clients = {
+        DecryptRest.class,
     InitiativeRestClient.class,
     PaymentRestClient.class,
     TransactionRestClient.class
