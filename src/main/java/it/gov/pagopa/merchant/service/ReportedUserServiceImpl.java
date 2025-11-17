fdfc57a7a0d843a9f4be86da2f3200ebb0c985f3
leonardomagnante <lmagnant@nttdata.com>
2025-11-17 10:00:05 +0100

    RDB-383 fix error message

--- src/main/java/it/gov/pagopa/merchant/service/ReportedUserServiceImpl.java
+++ src/main/java/it/gov/pagopa/merchant/service/ReportedUserServiceImpl.java
@@ -61,8 +61,16 @@ public class ReportedUserServiceImpl implements ReportedUserService {
             if (trxList.isEmpty() || trxList.getFirst() == null ) {
                 return ReportedUserCreateResponseDTO.ko(ReportedUserExceptions.USERID_NOT_FOUND);
             }
+
             trxList = trxList.stream()
                     .filter(trx -> ALLOWED_TRANSACTION_STATUSES.contains(trx.getStatus()))
+                    .toList();
+
+            if (trxList.isEmpty() || trxList.getFirst() == null ) {
+                return ReportedUserCreateResponseDTO.ko(ReportedUserExceptions.USERID_NOT_FOUND);
+            }
+
+            trxList = trxList.stream()
                     .filter(trx -> trx.getInitiatives() != null && trx.getInitiatives().contains(initiativeId))
                     .filter(trx -> merchantId.equals(trx.getMerchantId()))
                     .toList();
