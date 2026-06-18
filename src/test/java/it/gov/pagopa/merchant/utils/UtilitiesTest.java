afe266d0d8f3c9220c3e6ff8c1e10f9d077550a8
Giuseppe-La-Manna <Giuseppe.LaManna@emeal.nttdata.com>
2026-06-18 15:27:24 +0200

    BND-995 Updated tests

--- src/test/java/it/gov/pagopa/merchant/utils/UtilitiesTest.java
+++ src/test/java/it/gov/pagopa/merchant/utils/UtilitiesTest.java
@@ -1,11 +1,14 @@
 package it.gov.pagopa.merchant.utils;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
 
 class UtilitiesTest {
 
@@ -23,4 +26,27 @@ class UtilitiesTest {
 
         assertEquals(result, PageRequest.of(0, 15, Sort.by("updateDate")));
     }
+
+    @ParameterizedTest
+    @CsvSource({
+            "'https://www.test-imp.it', 'test-imp.it'",
+            "'https://www.test.it', 'test.it'",
+            "'https://test.it', test.it",
+            "'http://test.it', test.it",
+            "'www.test.it', test.it",
+            "'https://example.com/', 'example.com'",
+            "'https://  exa  mpl  e.c om /', 'example.com'",
+            "'HTTPS://WWW.EXAMPLE.COM', 'example.com'"
+    })
+    void givenFullWebsiteWhenSanitizeDomainThenReturnSanitizedDomain(String website, String expectedResult) {
+        String result = Utilities.sanitizeDomain(website);
+        assertEquals(expectedResult, result);
+    }
+
+    @Test
+    void givenNullWebsiteWhenSanitizeDomainThenReturnNull() {
+        String result = Utilities.sanitizeDomain(null);
+        assertNull(result);
+    }
+
 }
