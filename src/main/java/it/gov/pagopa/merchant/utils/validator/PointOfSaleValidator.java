7b3416369b69e09c52c2b9c9803e94de505a2127
v1tt0ri0Alt <valtieri@emeal.nttdata.com>
2026-07-01 16:22:34 +0200

    modified REGEX_LINK in PointOfSaleValidator

--- src/main/java/it/gov/pagopa/merchant/utils/validator/PointOfSaleValidator.java
+++ src/main/java/it/gov/pagopa/merchant/utils/validator/PointOfSaleValidator.java
@@ -26,7 +26,7 @@ public class PointOfSaleValidator {
     private final Validator validator;
 
     private static final String REGEX_PHONE = "^\\+?\\d{7,15}$";
-    private static final String REGEX_LINK = "^(https:\\/\\/)?(www\\.)?(?=[a-zA-Z0-9.-]*[a-zA-Z])((?!-)[a-zA-Z0-9-]+(?<!-)\\.)+[a-zA-Z]{2,6}(\\/[a-zA-Z0-9#_.-]*)*(\\?[a-zA-Z0-9_.-]+=[a-zA-Z0-9_.-]*(?:\\&[a-zA-Z0-9_.-]+=[a-zA-Z0-9_.-]*)*)?$";
+    private static final String REGEX_LINK = "^https:\\/\\/(?:[a-zA-Z0-9](?:[a-zA-Z0-9-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,}(?:\\/[^\\s]*)?$";
     private static final String REGEX_EMAIL = "^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$";
 
     private final int posDefaultMaxSizeRequest;
